47fdcc251a6bf2bcf3ebfaf38f361c8f8b7223e1
Aditya Naik <naik.aditya@gmail.com>
2018-10-29 16:53:05 +0100

    Added readme, added first test, added code to the atm.rb file to pass first test

---
+++ README.md
@@ -0,0 +1,7 @@
+ATM Challenge
+
+User stories:
+
+ As a User       
+ In order to make an withdrawal      
+ The ATM needs to have funds
---
+++ spec/atm_spec.rb
@@ -0,0 +1,8 @@
+require './lib/atm.rb'
+
+describe Atm do
+    it 'has USD 1000 on initialize' do
+        expect(subject.funds).to eq 1000        
+    end
+
+end
