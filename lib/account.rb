c2051fcd1d92028aa25ff76cc14c45746c500cce
Aditya Naik <naik.aditya@gmail.com>
2018-10-31 13:36:36 +0100

    create account class and initialize

---
+++ lib/account.rb
@@ -0,0 +1,14 @@
+
+
+class Account
+    attr_accessor :pin_code, :balance,  :account_status, :exp_date 
+    
+    def initialize ()
+    @pin_code = '1234'
+    @balance = 100
+    @ account_status = :active
+    @exp_date = '12/21'
+    end
+
+    
+end
