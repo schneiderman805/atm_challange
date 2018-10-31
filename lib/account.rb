f8c9dd68b9538507b4826654fab06146c2da098d
Aditya Naik <naik.aditya@gmail.com>
2018-10-31 15:38:21 +0100

    Added a test to determine expiry date, updated code accordingly to pass the test

--- lib/account.rb
+++ lib/account.rb
@@ -2,12 +2,17 @@
 
 class Account
     attr_accessor :pin_code, :balance,  :account_status, :exp_date 
+    STANDARD_VALIDITY_YRS = 5
     
     def initialize ()
     @pin_code = rand(1000..9999)
     @balance = 100
     @account_status = :active
-    @exp_date = '12/21'
+    @exp_date = set_expiry_date
+    end
+
+    def set_expiry_date 
+        Date.today.next_year(Account::STANDARD_VALIDITY_YRS).strftime('%m/%y')
     end
     
 end
