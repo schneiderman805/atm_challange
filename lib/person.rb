0273be5ed03d5d46853cd3f765526e00f1386f02
William Schneiderman <william.m.schneiderman@gmail.com>
2018-11-01 11:05:50 +0100

    added test for account and added code

--- lib/person.rb
+++ lib/person.rb
@@ -1,9 +1,12 @@
+require 'account.rb'
+
 class Person
     attr_accessor :name, :cash, :account
      
     def initialize (attrs = {})
     set_owner(attrs[:name])
     set_cash(attrs[:cash])
+    create_account()
     end
 
     def set_owner(obj)
@@ -15,4 +18,10 @@ class Person
     def set_cash(cash)
         cash == nil ? @cash = 0 : @cash = cash 
     end
+    def create_account()
+        @account = Account.new(owner: 'Thomas')
+        #account == nil ? set_account : @account = account
+    
+    end
+    
 end
--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -16,4 +16,10 @@ describe Person do
     it 'is expected to have :account attribute' do
         expect(subject.account).to be nil
     end
+    describe 'can create an Account' do
+        before { subject.create_account }
+        it 'of Account class' do
+            expect(subject.account).to be_an_instance_of Account
+        end
+    end
 end
